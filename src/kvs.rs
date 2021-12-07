21f9b2a5a04e2ece547f0c322de3b8079dd5e90b
banana-GK <tomsawyer404@outlook.com>
2021-12-07 10:19:32 +0800

    test: Meke the tests compile

---
+++ src/kvs.rs
@@ -0,0 +1,19 @@
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        unimplemented!();
+    }
+
+    pub fn set(&self, key: String, val: String) {
+        unimplemented!();
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        unimplemented!();
+    }
+
+    pub fn remove(&self, key: String) {
+        unimplemented!();
+    }
+}
