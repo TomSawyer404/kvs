2de4d8c8aa851e76bdc9ea0e242185f7a18ef5e8
banana-GK <tomsawyer404@outlook.com>
2021-12-18 19:50:45 +0800

    feat(logic): store index in memory only
    Frist step to rebuild the logic

--- src/kv.rs
+++ src/kv.rs
@@ -4,13 +4,16 @@ use serde_json::Deserializer;
 use std::{
     collections::HashMap,
     fs::{create_dir_all, File, OpenOptions},
-    io::{BufReader, BufWriter},
+    io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write},
+    ops::Range,
     path::{Path, PathBuf},
 };
 
 /// The `KvStore` stores string key/value pairs.
 ///
-/// Key/value pairs are stores in a `HashMap` in memory and not persisted to disk.
+/// Key/value pairs are stores in a `HashMap` in memory and persisted to disk
+/// using a Write-Ahead Log. A `HashMap` in memory stores the keys and the value
+/// location for fast query.
 ///
 /// Example:
 ///
@@ -23,9 +26,12 @@ use std::{
 /// ```
 #[derive(Debug)]
 pub struct KvStore {
-    map: HashMap<String, String>,
     path: PathBuf,
-    wal_writer: BufWriter<File>,
+
+    // write-head log
+    log_writer: BufWriterWithPos<File>,
+    log_reader: BufReaderWithPos<File>,
+    index: HashMap<String, CommandPos>,
 }
 
 impl KvStore {
@@ -37,58 +43,76 @@ impl KvStore {
 
         let mut wal_path = path.clone();
         wal_path.push("dblog.txt");
-        let log = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .append(true)
-            .create(true)
-            .open(&wal_path)?;
 
-        Ok(KvStore {
-            map: HashMap::new(),
+        let mut log_writer = BufWriterWithPos::new(
+            OpenOptions::new()
+                .create(true)
+                .read(true)
+                .append(true)
+                .open(&wal_path)?,
+        )?;
+
+        // Set pos to end of the file
+        log_writer.seek(SeekFrom::End(0))?;
+
+        let mut log_reader = BufReaderWithPos::new(File::open(wal_path)?)?;
+
+        let mut store = KvStore {
             path,
-            wal_writer: BufWriter::<File>::new(log),
-        })
+            log_reader,
+            log_writer,
+            index: HashMap::new(),
+        };
+        store.load_from_wal();
+
+        return Ok(store);
     }
 
     /// Sets the value of a string key to a string.
     /// If the key already exists, the previous value will be overwritten.
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        self.map.insert(key.clone(), value.clone());
-        let set_command = SetCommand::new(key, value);
-        serde_json::to_writer(&mut self.wal_writer, &set_command)?;
+        let cmd = SetCommand::new(key, value);
+        let pos = self.log_writer.pos;
+        serde_json::to_writer(&mut self.log_writer.writer, &cmd)?;
+        self.log_writer.flush()?;
+        self.index
+            .insert(cmd.key, (pos..self.log_writer.pos).into());
         Ok(())
     }
 
     /// Gets the string value of a given string key.
     /// Returns `None` if the given key does not exist.
-    pub fn get(&self, key: String) -> Result<Option<String>> {
-        if let Some(x) = self.map.get(&key) {
-            Ok(Some(x.clone()))
-        } else {
-            Err(KvsError::KeyNotFound)
-        }
+    pub fn get(&self, _key: String) -> Result<Option<String>> {
+        unimplemented!();
+        //if let Some(x) = self.map.get(&key) {
+        //    Ok(Some(x.clone()))
+        //} else {
+        //    Err(KvsError::KeyNotFound)
+        //}
     }
 
     /// Remove a given key.
-    pub fn remove(&mut self, key: String) -> Result<()> {
-        if let None = self.map.remove(&key) {
-            Err(KvsError::KeyNotFound)
-        } else {
-            Ok(())
-        }
+    pub fn remove(&mut self, _key: String) -> Result<()> {
+        unimplemented!();
+        //if let None = self.map.remove(&key) {
+        //    Err(KvsError::KeyNotFound)
+        //} else {
+        //    Ok(())
+        //}
     }
-}
 
-fn load_from_wal(wal_path: impl AsRef<Path>) -> Result<HashMap<String, String>> {
-    let mut map = HashMap::new();
-    let reader = BufReader::new(File::open(wal_path)?);
-    let stream = Deserializer::from_reader(reader).into_iter::<SetCommand>();
-    for set_cmd in stream {
-        let set_cmd = set_cmd?;
-        map.insert(set_cmd.key, set_cmd.value);
+    fn load_from_wal(&mut self) -> Result<()> {
+        let mut pos = self.log_reader.seek(SeekFrom::Start(0))?;
+        let mut stream = Deserializer::from_reader(&mut self.log_reader).into_iter::<SetCommand>();
+
+        while let Some(set_command) = stream.next() {
+            let new_pos = stream.byte_offset() as u64;
+            self.index.insert(set_command?.key, (pos..new_pos).into());
+            pos = new_pos;
+        }
+
+        Ok(())
     }
-    Ok(map)
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -111,5 +135,84 @@ struct SetCommand {
 impl SetCommand {
     fn new(key: String, value: String) -> SetCommand {
         SetCommand { key, value }
+    }
+}
+
+/// Represents the position and length of a json-serialized command in wal-log
+#[derive(Debug)]
+struct CommandPos {
+    pos: u64,
+    len: u64,
+}
+
+impl From<Range<u64>> for CommandPos {
+    fn from(range: Range<u64>) -> Self {
+        CommandPos {
+            pos: range.start,
+            len: range.end - range.start,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct BufReaderWithPos<R: Read + Seek> {
+    reader: BufReader<R>,
+    pos: u64,
+}
+
+impl<R: Read + Seek> BufReaderWithPos<R> {
+    fn new(mut inner: R) -> Result<Self> {
+        let pos = inner.seek(SeekFrom::Current(0))?;
+        Ok(BufReaderWithPos {
+            reader: BufReader::new(inner),
+            pos,
+        })
+    }
+}
+
+impl<R: Read + Seek> Read for BufReaderWithPos<R> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        self.reader.read(buf)
+    }
+}
+
+impl<R: Read + Seek> Seek for BufReaderWithPos<R> {
+    fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
+        self.reader.seek(pos)
+    }
+}
+
+#[derive(Debug)]
+struct BufWriterWithPos<W: Write + Seek> {
+    writer: BufWriter<W>,
+    pos: u64,
+}
+
+impl<W: Write + Seek> BufWriterWithPos<W> {
+    fn new(mut inner: W) -> Result<Self> {
+        let pos = inner.seek(SeekFrom::Current(0))?;
+        Ok(BufWriterWithPos {
+            writer: BufWriter::new(inner),
+            pos,
+        })
+    }
+}
+
+impl<W: Write + Seek> Write for BufWriterWithPos<W> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let len = self.writer.write(buf)?;
+        self.pos += len as u64;
+        Ok(len)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.writer.flush()
+    }
+}
+
+impl<W: Write + Seek> Seek for BufWriterWithPos<W> {
+    fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
+        self.pos = self.writer.seek(pos)?;
+        Ok(self.pos)
     }
 }
